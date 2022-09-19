b425132375b51729b221d7aa9c019bc88eb42049
lmenjoulet <lmenjoulet@laposte.net>
2022-09-19 19:06:58 +0200

    enabled nvidia modesetting

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }: {
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      vaapiVdpau
-      libvdpau-va-gl
-      libva-utils
-    ];
+  hardware = {
+    nvidia.modesetting.enable = true;
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+        libva-utils
+      ];
+    };
   };
 }
